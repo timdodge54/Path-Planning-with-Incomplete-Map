5785585bf189ee8bc35bd1ca3a11a435300a909f
Tim Dodge <tim.dodge64@gmail.com>
2022-11-07 16:39:53 -0700

    adding map

--- src/reinforcement_planning/launch/launch_nodes.launch.py
+++ src/reinforcement_planning/launch/launch_nodes.launch.py
@@ -2,28 +2,40 @@ from launch import LaunchDescription
 import launch_ros.actions
 from ament_index_python.packages import get_package_share_directory
 
+from launch import LaunchDescription
+import launch_ros.actions
+
 
 def generate_launch_description():
     # Parameters
-    lifecycle_nodes = ['map_saver', 'map_server']
+    lifecycle_nodes = ['map_server']
     use_sim_time = True
     autostart = True
     save_map_timeout = 2000
     free_thresh_default = 0.25
     occupied_thresh_default = 0.65
-    share = get_package_share_directory('reinforcement_planning')
-    file_loc = f"{share}/map.yaml"
 
     # Nodes launching commands
-    map_loader_node = launch_ros.actions.Node(
-        package='reinforcement_planning',
-        executable='map_loader_cmd',
-        name='map_loader',
-        parameters=[{'map_path': file_loc}],
+    start_map_saver_server_cmd = launch_ros.actions.Node(
+            package='nav2_map_server',
+            executable='map_server',
+            output='screen',
+            emulate_tty=True,  # https://github.com/ros2/launch/issues/188
+            parameters=[{'yaml_filename': get_package_share_directory('reinforcement_planning') + '/config/map.yaml'}],
     )
+    start_lifecycle_manager_cmd = launch_ros.actions.Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager',
+            output='screen',
+            emulate_tty=True,  # https://github.com/ros2/launch/issues/188
+            parameters=[{'use_sim_time': use_sim_time},
+                        {'autostart': autostart},
+                        {'node_names': lifecycle_nodes}])
 
     ld = LaunchDescription()
-    ld.add_action(map_loader_node)
 
+    ld.add_action(start_map_saver_server_cmd)
+    ld.add_action(start_lifecycle_manager_cmd)
 
     return ld
