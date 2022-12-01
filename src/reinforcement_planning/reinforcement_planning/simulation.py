3d431b5aaf8deaa7ea8c917c8007a6bc0a73b3f9
Jarom Burr <jaromburr@hotmail.com>
2022-11-30 20:06:48 -0700

    new a-star algo

--- src/reinforcement_planning/reinforcement_planning/simulation.py
+++ src/reinforcement_planning/reinforcement_planning/simulation.py
@@ -32,94 +32,120 @@ class AStarPlanner:
         self.max_x, self.max_y = 0, 0
         self.obstacle_map = None
         self.x_width, self.y_width = 0, 0
-        self.motion = self.get_l()
+        self.motion = self.get_motion_model()
         self.calc_obstacle_map(ox, oy)
-        self.nodes = [[None for x in range(resolution)] for y in range(resolution)]
-        self.pq = PriorityQueue(100000)
-    
+
     class Node:
-        def __init__(self, x, y, cost, steps, parent_index, isObstacle=False):
+        def __init__(self, x, y, cost, parent_index):
             self.x = x  # index of grid
             self.y = y  # index of grid
             self.cost = cost
-            self.steps = steps
             self.parent_index = parent_index
-            self.isObstacle = isObstacle
-
-        def __gt__(self, other):
-            return self.cost > other.cost
-
-        def __lt__(self, other):
-            return self.cost < other.cost 
-
-        def __eq__(self, other):
-            if(other == None):
-                return False
-            return self.cost == other.cost  
 
         def __str__(self):
-            if(self.isObstacle):
-                return str('x')
-            return str(f'{self.x},{self.y}, {self.parent_index}|')
+            return str(self.x) + "," + str(self.y) + "," + str(
+                self.cost) + "," + str(self.parent_index)
 
-    def planning(self, sx, sy, gx, gy, ox, oy):
+    def planning(self, sx, sy, gx, gy):
+        """
+        A star path search
 
-        
-        start_node = self.Node(sx + 10, sy + 10, self.calc_heuristic(gx + 10, gy + 10, sx + 10, sy + 10), 0, (-1,-1))
-        current = start_node
+        input:
+            s_x: start x position [m]
+            s_y: start y position [m]
+            gx: goal x position [m]
+            gy: goal y position [m]
 
-        self.nodes[start_node.x][start_node.y] = start_node
+        output:
+            rx: x position list of the final path
+            ry: y position list of the final path
+        """
 
-        for i in range(len(ox)):
-            self.nodes[ox[i] + 10][oy[i]+ 10] = self.Node(ox[i] + 10, oy[i] + 10, 0, 0, (0, 0), True)
+        start_node = self.Node(self.calc_xy_index(sx, self.min_x),
+                               self.calc_xy_index(sy, self.min_y), 0.0, -1)
+        goal_node = self.Node(self.calc_xy_index(gx, self.min_x),
+                              self.calc_xy_index(gy, self.min_y), 0.0, -1)
 
-        j = 0
-        while(current.x != (gx + 10) or current.y != (gy + 10)):
-            j += 1
+        open_set, closed_set = dict(), dict()
+        open_set[self.calc_grid_index(start_node)] = start_node
 
+        while 1:
+            if len(open_set) == 0:
+                print("Open set is empty..")
+                break
+
+            c_id = min(
+                open_set,
+                key=lambda o: open_set[o].cost + self.calc_heuristic(goal_node,
+                                                                     open_set[
+                                                                         o]))
+            current = open_set[c_id]
+
+            # show graph
+            # if show_animation:  # pragma: no cover
+            #     plt.plot(self.calc_grid_position(current.x, self.min_x),
+            #              self.calc_grid_position(current.y, self.min_y), "xc")
+            #     # for stopping simulation with the esc key.
+            #     plt.gcf().canvas.mpl_connect('key_release_event',
+            #                                  lambda event: [exit(
+            #                                      0) if event.key == 'escape' else None])
+            #     if len(closed_set.keys()) % 10 == 0:
+            #         plt.pause(0.001)
+
+            if current.x == goal_node.x and current.y == goal_node.y:
+                print("Find goal")
+                goal_node.parent_index = current.parent_index
+                goal_node.cost = current.cost
+                break
+
+            # Remove the item from the open set
+            del open_set[c_id]
+
+            # Add it to the closed set
+            closed_set[c_id] = current
+
+            # expand_grid search grid based on motion model
             for i, _ in enumerate(self.motion):
-                x = current.x + self.motion[i][0]
-                y = current.y + self.motion[i][1]
-                step = self.motion[i][2]
-                if(self.nodes[x][y] is None): # Visited
-                    node = self.Node(x, y, current.steps + self.calc_heuristic(gx, gy, x, y), current.steps + step, (current.x, current.y))
-                    self.nodes[node.x][node.y] = node
-                    self.pq.put(node)
-            
-            current = self.pq.get()    
-        
-        self.nodes[current.x][current.y] = node
-        
-        # for row in self.nodes:
-        #     for col in row:
-        #         if col == None:
-        #             print('N', end= ' ')
-        #         else:
-        #             print(col, end=' ')
-        #     print()
+                node = self.Node(current.x + self.motion[i][0],
+                                 current.y + self.motion[i][1],
+                                 current.cost + self.motion[i][2], c_id)
+                n_id = self.calc_grid_index(node)
 
-        return self.calc_final_path(current)
+                # If the node is not safe, do nothing
+                if not self.verify_node(node):
+                    continue
 
-    def calc_final_path(self, goal_node):
+                if n_id in closed_set:
+                    continue
+
+                if n_id not in open_set:
+                    open_set[n_id] = node  # discovered a new node
+                else:
+                    if open_set[n_id].cost > node.cost:
+                        # This path is the best until now. record it
+                        open_set[n_id] = node
+
+        rx, ry = self.calc_final_path(goal_node, closed_set)
+
+        return rx, ry 
+
+    def calc_final_path(self, goal_node, closed_set):
         # generate final course
-        rx = [goal_node.x - 10]
-        ry = [goal_node.y - 10]
+        rx, ry = [self.calc_grid_position(goal_node.x, self.min_x)], [
+            self.calc_grid_position(goal_node.y, self.min_y)]
         parent_index = goal_node.parent_index
-        startTime = time.time()
-        while parent_index != (-1, -1):
-            n = self.nodes[int(parent_index[0])][int(parent_index[1])]
-            if n is None:
-                return None, None
-            rx.append(n.x - 10)
-            ry.append(n.y - 10)
+        while parent_index != -1:
+            n = closed_set[parent_index]
+            rx.append(self.calc_grid_position(n.x, self.min_x))
+            ry.append(self.calc_grid_position(n.y, self.min_y))
             parent_index = n.parent_index
 
         return rx, ry
 
     @staticmethod
-    def calc_heuristic(x1, y1, x2, y2):
+    def calc_heuristic(n1, n2):
         w = 1.0  # weight of heuristic
-        d = w * math.hypot(x2 - x1, y2 - y1)
+        d = w * math.hypot(n1.x - n2.x, n1.y - n2.y)
         return d
 
     def calc_grid_position(self, index, min_position):
@@ -139,16 +165,35 @@ class AStarPlanner:
     def calc_grid_index(self, node):
         return (node.y - self.min_y) * self.x_width + (node.x - self.min_x)
 
+    def verify_node(self, node):
+        px = self.calc_grid_position(node.x, self.min_x)
+        py = self.calc_grid_position(node.y, self.min_y)
+
+        if px < self.min_x:
+            return False
+        elif py < self.min_y:
+            return False
+        elif px >= self.max_x:
+            return False
+        elif py >= self.max_y:
+            return False
+
+        # collision check
+        if self.obstacle_map[node.x][node.y]:
+            return False
+
+        return True
+
     def calc_obstacle_map(self, ox, oy):
 
         self.min_x = round(min(ox))
         self.min_y = round(min(oy))
         self.max_x = round(max(ox))
         self.max_y = round(max(oy))
-        #print("min_x:", self.min_x)
-        #print("min_y:", self.min_y)
-        #print("max_x:", self.max_x)
-        #print("max_y:", self.max_y)
+        # print("min_x:", self.min_x)
+        # print("min_y:", self.min_y)
+        # print("max_x:", self.max_x)
+        # print("max_y:", self.max_y)
 
         self.x_width = round((self.max_x - self.min_x) / self.resolution)
         self.y_width = round((self.max_y - self.min_y) / self.resolution)
@@ -169,7 +214,7 @@ class AStarPlanner:
                         break
 
     @staticmethod
-    def get_l():
+    def get_motion_model():
         # dx, dy, cost
         motion = [[1, 0, 1],
                   [0, 1, 1],
@@ -217,10 +262,10 @@ class Simulation:
         x_Dot = -aveVel * math.sin(theta - math.pi / 2)
         y_Dot = aveVel * math.cos(theta - math.pi / 2)
         theta_Dot = (v_right - v_left) / (self.robot_radius*2)
+
         x_new = x + x_Dot * dt
         y_new = y + y_Dot * dt
         theta_new = (theta + theta_Dot * dt) % (2*math.pi)
-
 
         return x_new, y_new, theta_new
 
@@ -258,11 +303,8 @@ class Simulation:
         self.ox = ox
         self.oy = oy
         self.map = map
-        while True:
-            a_star = AStarPlanner(ox, oy, self.grid_size, self.robot_radius)
-            rx, ry = a_star.planning(self.sx, self.sy, self.gx, self.gy, ox, oy)
-            if rx != None:
-                break
+        a_star = AStarPlanner(ox, oy, self.grid_size, self.robot_radius)
+        rx, ry = a_star.planning(self.sx, self.sy, self.gx, self.gy)
         rx.reverse()
         ry.reverse()
         self.rx = rx
@@ -372,7 +414,7 @@ class Simulation:
         xpoints = x + radius * np.cos( angle )
         ypoints = y + radius * np.sin( angle )
         self.robot, = plt.plot(xpoints, ypoints, marker = ".", color = 'r')
-        self.robotAngle, = plt.plot(x + radius * np.cos( theta ), y + radius * np.sin( theta ), marker = ".", color = 'b')
+        self.robotAngle, = plt.plot(x + radius * math.cos( theta ), y + radius * math.sin( theta ), marker = ".", color = 'b')
 
     def showPath(self):
         self.print()
@@ -398,7 +440,7 @@ class Simulation:
 
         path_x, path_y = self.getPath()
         g_x, g_y = self.getGoal()
-        return [path_x, path_y, g_x, g_y, self.getObstacle(), self.getTheta(), self.tx[-1], self.ty[-1], self.theta[-1]]
+        return [path_x, path_y, g_x, g_y, self.getObstacle(), self.getTheta(), self.ix[-1], self.iy[-1], self.theta[-1]]
 
 def main():
     print(__file__ + " start!!")
@@ -407,11 +449,14 @@ def main():
     sy = 10  # [m]
     gx = 50  # [m]
     gy = 50  # [m]
-    grid_size = 71  # [m]
+    grid_size = 2  # [m]
     robot_radius = 1.0  # [m]
     obstacle_count = 25
 
     sim = Simulation(robot_radius, grid_size, obstacle_count, sx, sy, gx, gy)
+    for i in range(10):
+        sim.step([0.5,0.5])
+    sim.showPath()
 
     agent = Agent(
     alpha=0.000025,
@@ -424,7 +469,7 @@ def main():
     n_actions=2,
     action_range=1
     )
-
+    agent.load_models()
     print(T.cuda.is_available())
     np.random.seed(0)
 
